308016d68caf74b2f29a79fd2a8479d288654ffe
jruel006 <jruel006@ucr.edu>
2015-06-17 13:40:37 -0700

    NetworkTester now has basic testing capabilities for local IP, primary IP, DNS and internet failure with timestamp

--- http.cc
+++ http.cc
@@ -8,12 +8,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 #include <cstring>
 #include <cstdlib>
 #include <stdlib.h>
 #include <ctime>
-#include </usr/include/curl/curl.h>
+#include <curl/curl.h>
+#include <sstream>
 #include <stdio.h>
+#include <assert.h>
+#include <stdio.h>
+#include <algorithm>
 
 //==========Headers for different OS's for sleep syscall===========
 #ifdef __APPLE__
@@ -33,6 +38,7 @@
 #endif
 //=================================================================
 
+// custom sleep function used for different operation systems
 void mySleep(int sleepMs){
 
 	#ifdef LINUX
@@ -61,84 +67,174 @@ std::string translate(char * data){
     return std::string(data);
 }
 
-size_t write_data(char *ptr, size_t size, size_t nmemb, void *userdata){
+static size_t printHandle(char * html, size_t size ,size_t nmembm, void * userdata){		
 	
-	return size * nmemb;
+	return size * nmembm;
+}
+
+void http_get_request(const std::string & name, const std::string & domain, unsigned & interval, unsigned & timeout){
+	
+	CURL * curl = curl_easy_init(); // intialize curl
+	CURLcode res = CURLE_OK; // bool value used in error checking	
+	std::ofstream outputfileStream; // declares an output stream
+	std::string outfile = name + "REQUESTS.txt"; // appends `REQUESTS,txt` to file name
+	outputfileStream.open(outfile.c_str()); // creates an outfile with `REQUESTS.txt` extension in current directory
+	unsigned counter = 1; // counter that will keep track of number of http requests sent
+	
+	
+	// ill put condition here to check for web flags later
+	while(1){
+		
+		char * localIP; // = '\0'; // char * that stores the local IP address
+		char * primaryIP; // = '\0'; // char * that sores the primary IP address
+		std::cout << "[" << counter << "] request" << std::endl; // for the user to see that work is actually being done on the screen
+		
+		// error checks curl initialization
+		if (!curl){
+			// need to put some sort of flags if curl initialization fails
+			// to tell web server that it needs to be fixed on that computer
+			
+			fprintf(stderr, "curl initialization failure\n"); // if error occurs, prints to stderr
+			outputfileStream << "\n\"curl initialization failure\"\n"; // if error occurs, prints to txt file
+			return;
+		}
+		//=====================================
+		// configure curl member functions here
+		//=====================================
+		
+		// prints out the request number
+		outputfileStream <<"\n\"REQUEST[" << counter << "]\","; // if intialization is okay print request #
+		
+		// sets the 'printHandle` function as the pring handler function
+		if((res = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, printHandle)) != CURLE_OK){
+			fprintf(stderr,"%s\n", curl_easy_strerror(res)); // if error occurs initalizing write handle function, output error
+			outputfileStream << "\"" << curl_easy_strerror(res) << "\""; // output same error, but to txt file
+			counter++;
+			continue;
+		}
+		else { outputfileStream << "\"CURLOPT_WRITEFUNCTION is OK\","; }  // if setting print functions is good then print success code and proceed
+
+		// sets the URL to be used
+		if ((res = curl_easy_setopt(curl, CURLOPT_URL, domain.c_str())) != CURLE_OK){
+			fprintf(stderr, "%s\n", curl_easy_strerror(res)); // if error occurs, prints to stderr
+			outputfileStream << "\"" << curl_easy_strerror(res) << "\""; // if error occurs prints to txt file
+			counter++;
+			continue;
+		}
+		else {outputfileStream << "\"CURLOPT_URL is OK\",";} // if setting url is good print success code
+		
+		// sets up an HTTPGET request
+		if ((res = curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L)) != CURLE_OK){
+			fprintf(stderr, "%s\n", curl_easy_strerror(res)); // if error occurs, print to stderr
+			outputfileStream << "\"" << curl_easy_strerror(res) << "\""; // if error occurs print to txt file
+			counter++;
+			continue;
+		}
+		else { outputfileStream << "\"HTTP get request OK\",";} // if get request succeeds then proceed	
+		
+		// sets the timeout variable from paramter
+		if ((res = curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout)) != CURLE_OK) {
+			fprintf(stderr, "%s\n", curl_easy_strerror(res)); // if error occurs, prints to stderr
+			outputfileStream << "\"" << curl_easy_strerror(res) << "\""; // if error occurs, print to txt file
+			counter++;
+			continue;
+		}
+		else { outputfileStream << "\"Timeout value OK\",";} // if timeout settings succeed then proceed
+		
+		//=====================================
+		// execute the configurations
+		//=====================================
+		// performs the request
+		if ((res = curl_easy_perform(curl)) != CURLE_OK){	
+			
+			/*switch (res){
+        			case CURLE_COULDNT_CONNECT:
+					outputfileStream << "\"" << curl_easy_strerror(res) << "\","; // does the same but prints to txt file
+        				break;
+				case CURLE_COULDNT_RESOLVE_HOST:	
+					outputfileStream << "\"" << curl_easy_strerror(res) << "\","; // does the same but prints to txt file
+        				break;
+				case CURLE_COULDNT_RESOLVE_PROXY:
+					outputfileStream << "\"" << curl_easy_strerror(res) << "\","; // does the same but prints to txt file
+					std::cout << "Internet dose not exist\n";
+					break;
+				default: 
+					break;
+    			}*/	
+			// this is where the error output goes
+			
+			outputfileStream << "\"" << curl_easy_strerror(res) << "\","; // does the same but prints to txt file
+			fprintf(stderr, "%s\n", curl_easy_strerror(res)); // if cannot reslove domain name, print to stdout
+			counter++;
+			mySleep(interval);
+			continue;
+		}
+		else {outputfileStream << "\"Domain name resolved\",";} // if domain name resolved, proceed
+		
+		//==================================
+		// Gets Local and Primary IP address
+		//==================================
+		//  gets the local IP addres
+		bool local = true;
+		bool primary = true;;
+		if((res = curl_easy_getinfo(curl, CURLINFO_LOCAL_IP, &localIP)) != CURLE_OK){
+			fprintf(stderr, "%s\n", curl_easy_strerror(res)); // if local IP not found, print to stderr
+			outputfileStream << "\"" << curl_easy_strerror(res) << "\""; // if local IP not found, print to txt
+			local = false; // set print bool to false to not print the local IP
+			counter++; // increment counter for request number at the top
+			continue; // continue to next iteration of while loop
+		}
+		if ((res = curl_easy_getinfo(curl, CURLINFO_PRIMARY_IP, &primaryIP)) != CURLE_OK) {
+			fprintf(stderr, "%s\n", curl_easy_strerror(res)); // if primary IP not found, print to stderr
+			outputfileStream << "\"" << curl_easy_strerror(res) << "\""; // if primary IP not found, print to txt
+			primary = false; // set print bool to false to not print the primary IP
+			counter++; // increment counter for request number at the toop
+			continue; // contine to next iteration of while loop
+		}
+		if (local){ // if attaining local IP address succeeded, print it
+			outputfileStream << "\"Local IP found: " << localIP << "\",";
+		}
+		if (primary){ // 	
+			outputfileStream << "\"Primary IP found: " << primaryIP << "\",";
+		}
+		outputfileStream.flush();
+			
+		time_t ltime; /* calendar time */
+		ltime=time(NULL); /* get current cal time */
+		std::string test = asctime( localtime(&ltime) ); // moves time stamp to a string
+		test.erase(std::remove(test.begin(), test.end(), '\n'), test.end()); // erases the \n at the end of the string
+		outputfileStream <<"\"" <<  test << "\""; // prints it out to txt file
+		/*
+		FILE * printFile = fopen(outfile.c_str(), "a+");
+		fprintf(printFile, "Local IP found: %s <---> Primary IP found: %s \n",localIP?localIP:"na", primaryIP?primaryIP:"na");	
+		*/
+		
+		//==========================================
+		// Sleep for alloted time given in parameter
+		//==========================================
+		mySleep(interval);
+		counter++;
+	}
+	//=====================================
+	// cleanup the curl stuff
+	//=====================================
+	outputfileStream.close(); // closes the outfile stream
+	curl_easy_cleanup(curl); // cleans up curl
 }
 
 int main(int argc, char * argv[]){
 	
-	unsigned counter = 0;
 	// name, domain, interval
-	if (argc < 4 || argc > 4){ // makes sure that the right parameters were used
+	if (argc < 5 || argc > 5){ // makes sure that the right parameters were used
 		std::cout << "You did not input the right parameters, try again\n";
 		return -1;
 	}
-	std::string name = translate(argv[1]);   // makes the `name` parameter a string
+	std::string name = translate(argv[1]);  // makes the `name` parameter a string
 	std::string domain = translate(argv[2]); // makes the `domain` parameter a string
-	unsigned interval = atoi(argv[3]); // converts char * to int to be used as a counter
-
-	std::string outfile = name + "REQUESTS.txt"; // appends `REQUESTS,txt` to string
-	std::ofstream outputfileStream; // declares an output stream buffer
-	outputfileStream.open(outfile.c_str()); // creates an outfile with `REQUESTS.txt` extension in current directory
+	unsigned interval = atoi(argv[3]); // converts char * to int to be used as a sleep timer
+	unsigned timeout = atoi(argv[4]); // converst char * to int to be used as timeout variable for requests
+	std::string outfile = name + "REQUESTS.txt"; // appends `REQUESTS,txt` to string	
+	http_get_request(name, domain, interval, timeout);
 	
-	// TODO: output on one line the data below separated by commas
-	// local IP address, time stamp, name of computer
-	
-	
-	for (unsigned i = 0; i < 1; i++){
-		
-		// output more stuff here
-		// output outcome here
-		
-		CURL * curl = curl_easy_init(); // initializes curl
-		CURLcode res = CURLE_OK; // says something is okay, not sure what though
-		char IP[100] = {0};	
-		
-		// error checks curl initialization
-		if (!curl){ 
-			fprintf(stderr, "curl initialization failure\n");
-			return -1;
-		}
-		
-		// sets the write_data as the 
-		if((res = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data)) != CURLE_OK){
-			fprintf(stderr, "%s\n", curl_easy_strerror(res));
-			return -1;
-		}
-			
-		//  
-		if((res = curl_easy_getinfo(curl, CURLINFO_LOCAL_IP, IP)) != CURLE_OK){
-			fprintf(stderr, "%s\n", curl_easy_strerror(res));
-			return -1;
-		}		
-		std::cout << "The IP address is: " << IP << std::endl;
-		
-		// sets url we will post to
-		if ((res = curl_easy_setopt(curl, CURLOPT_URL, domain.c_str())) != CURLE_OK){
-			fprintf(stderr, "%s\n", curl_easy_strerror(res));
-			return -1;
-		}
-		
-		// sets the post fields for the http post
-		if ((res = curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=juan&project=curl")) != CURLE_OK){
-			fprintf(stderr, "%s\n", curl_easy_strerror(res));
-			return -1;
-		}
-		
-		// does all the magic
-		if ((res = curl_easy_perform(curl)) != CURLE_OK){
-			
-			outputfileStream << curl_easy_strerror(res);
-			// this is where the error output goes
-			fprintf(stderr, "%s\n", curl_easy_strerror(res));
-			return -1;
-		}
-		
-		std::cout <<"\n" << "REQUEST[" << counter << "]: ";
-		mySleep(interval);
-	}
-	
-	outputfileStream.close();	// closes the outfile stream
 	return 0;
 }
